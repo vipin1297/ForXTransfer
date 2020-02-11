ee3072236997787d4e591c4e5ec9e3ec43ccf15d
Sri-Keerthna <srikeerthi2209@gmail.com>
2020-02-11 13:23:38 +0530

    List of Accounts api completed with logger and comments

--- src/main/java/com/spiralforge/ForXTransfer/config/SwaggerConfig.java
+++ src/main/java/com/spiralforge/ForXTransfer/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.demo.passencrypt")).paths(PathSelectors.any()).build();
+				.apis(RequestHandlerSelectors.basePackage("com.spiralforge.ForXTransfer")).paths(PathSelectors.any()).build();
 
 	}
 }
